049fc20a883becbe7662b72d9661213e54c286d6
harish30m <24mca32@kgisliim.ac.in>
2025-06-11 21:56:49 +0530

    Update Q10_ProductOfOddIntegers.java

--- Problem Solving in Java/Q10_ProductOfOddIntegers.java
+++ Problem Solving in Java/Q10_ProductOfOddIntegers.java
@@ -1,6 +1,23 @@
-package basic.to.intermediate;
+import java.util.Scanner;
 
-public class Q10_ProductOfOddIntegers {
+public class ProductOddNumbers 
+{
+    public static void main(String[] args) 
+	{
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the upper limit: ");
+        int limit = scanner.nextInt();
+        long product = 1;
+
+        for (int i = 1; i <= limit; i += 2) 
+	{
+            product *= i;
+        }
+
+        System.out.println("Product of Odd Integers (1 to " + limit + "): " + product);
+    }
+}
+
 	
 	/**
 	 * This program calculates and prints the product of odd numbers from 1 to 15.
@@ -19,4 +36,4 @@ public class Q10_ProductOfOddIntegers {
 
 	 */
 
-}
+
