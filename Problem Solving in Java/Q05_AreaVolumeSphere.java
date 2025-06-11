aaae95835c97bb73e4b797b1e15f29e2214858d1
harish30m <24mca32@kgisliim.ac.in>
2025-06-11 21:53:14 +0530

    Update Q05_AreaVolumeSphere.java

--- Problem Solving in Java/Q05_AreaVolumeSphere.java
+++ Problem Solving in Java/Q05_AreaVolumeSphere.java
@@ -1,6 +1,19 @@
-package basic.to.intermediate;
+import java.util.Scanner;
 
-public class Q05_AreaVolumeSphere {
+public class SphereAreaVolume 
+{
+    public static void main(String[] args) 
+	{
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter radius of the sphere: ");
+        double radius = scanner.nextDouble();
+        double surfaceArea = 4 * Math.PI * radius * radius;
+        double volume = (4.0 / 3) * Math.PI * Math.pow(radius, 3);
+        System.out.println("Surface Area: " + surfaceArea);
+        System.out.println("Volume: " + volume);
+    }
+}
+
 	
 	/**
 	 *  This program calculates the Volume and Surface Area of a sphere 
@@ -23,4 +36,4 @@ public class Q05_AreaVolumeSphere {
 	
 	 */
 
-}
+
