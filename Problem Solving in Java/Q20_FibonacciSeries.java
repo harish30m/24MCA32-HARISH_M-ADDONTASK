9dfc6a2e1b398ab6adbdddbb969f54c8d706ff80
harish30m <24mca32@kgisliim.ac.in>
2025-06-12 20:32:17 +0530

    Update Q20_FibonacciSeries.java

--- Problem Solving in Java/Q20_FibonacciSeries.java
+++ Problem Solving in Java/Q20_FibonacciSeries.java
@@ -1,24 +1,24 @@
-package basic.to.intermediate;
+import java.util.Scanner;
 
-public class Q20_FibonacciSeries {
-	/**
-	 * This program calculates and displays the first n numbers in the 
-	 Fibonacci sequence. Each number is the sum of the previous 
-	 two numbers.
-	 
-	 Code Flow 
-	Input: number of terms n
-	Initialize first two terms: f1 = 1, f2 = 1
-	Loop from i = 3 to n:
-		Find next term using f = f1 + f2
-		Shift values for next round
-	Display all terms
-	
-	Sample Input/Output
- 	Input:
-	How many Fibonacci terms? 10
-	Output:
-	1 1 2 3 5 8 13 21 34 55
-
-	 */
+public class Main 
+{
+    public static void main(String[] args) 
+	{
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many Fibonacci terms? ");
+        int n = sc.nextInt();
+        int f1 = 1, f2 = 1;
+        if (n >= 1) System.out.print(f1 + " ");
+        if (n >= 2) System.out.print(f2 + " ");
+        for (int i = 3; i <= n; i++) 
+	{
+            int f = f1 + f2;
+            System.out.print(f + " ");
+            f1 = f2;
+            f2 = f;
+        }
+        System.out.println();
+        sc.close();
+    }
 }
+
