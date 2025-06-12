098e836c432105866cd98883b88032610b88752b
harish30m <24mca32@kgisliim.ac.in>
2025-06-12 09:21:43 +0530

    Update Q10_ProductOfOddIntegers.java

--- Problem Solving in Java/Q10_ProductOfOddIntegers.java
+++ Problem Solving in Java/Q10_ProductOfOddIntegers.java
@@ -19,21 +19,6 @@ public class ProductOddNumbers
 }
 
 	
-	/**
-	 * This program calculates and prints the product of odd numbers from 1 to 15.
-		(Odd numbers: 1, 3, 5, 7, ..., 15)
-		
-		Problem Flow:
-		Start from 1 to 15
-		Increment by 2 to get odd numbers
-		Multiply all odd numbers
-		Print the final product
-		
-		Input / Output Example:
-		Output:
-		Product of odd numbers from 1 to 15 = 2027025
-		(Odd numbers: 1 × 3 × 5 × 7 × 9 × 11 × 13 × 15 = 2027025)
-
-	 */
+	
 
 
