7e9136a50ed274037ecc8b42ff0345c312f0ff9c
harish30m <24mca32@kgisliim.ac.in>
2025-06-12 20:23:05 +0530

    Update Q12_Histogram.java

--- Problem Solving in Java/Q12_Histogram.java
+++ Problem Solving in Java/Q12_Histogram.java
@@ -1,30 +1,25 @@
-package basic.to.intermediate;
+import java.util.Scanner;
 
-public class Q12_Histogram {
-	
-	/**
-	 * This program reads 5 numbers (each between 1 and 30) from the user.
-	 For each number, it prints a line with that many asterisks *, 
-	 forming a simple bar chart or histogram.
-	 
-	 Problem Flow:
-	Read 5 numbers from the user
-	Loop through each number
-	For each number:
-	Print the number
-	Print that many * using inner loop
-	
-	Sample Output:
-	Input:
-	Enter 5 Numbers in a same line: 
-	3 7 2 6 1
-	Output:
-	3 ***
- 	7 *******
- 	2 **
- 	6 ******
- 	1 *
- 	
-	 */
-
+public class Main 
+{
+    public static void main(String[] args) 
+	{
+        Scanner sc = new Scanner(System.in);
+        int[] numbers = new int[5];
+        System.out.println("Enter 5 numbers between 1 and 30:");
+        for (int i = 0; i < 5; i++) 
+	{
+            numbers[i] = sc.nextInt();
+        }
+        for (int num : numbers) 
+	{
+            System.out.print(num + " ");
+            for (int i = 0; i < num; i++) 
+	    {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
 }
