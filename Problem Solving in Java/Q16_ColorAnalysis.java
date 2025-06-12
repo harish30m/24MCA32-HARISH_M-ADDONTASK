4ae82221e9f948f68e93b65b6988fe121b3dcfcd
harish30m <24mca32@kgisliim.ac.in>
2025-06-12 20:28:19 +0530

    Update Q16_ColorAnalysis.java

--- Problem Solving in Java/Q16_ColorAnalysis.java
+++ Problem Solving in Java/Q16_ColorAnalysis.java
@@ -1,27 +1,26 @@
-package basic.to.intermediate;
+import java.util.Scanner;
 
-public class Q16_ColorAnalysis {
-	
-	/**
-	 *  This program uses a switch statement to examine the value of a 
-	 char-type variable called colour and prints one of the following
-	 messages, depending on the character assigned:
-		RED if either r or R is assigned to colour.
-		GREEN if either g or G is assigned to colour.
-		BLUE if either b or B is assigned to colour.
-		BLACK if any other character is assigned to colour.
-		
-		Problem Flow:
-		Prompt the user to enter a single character for the color code.
-		Use a switch statement to check the character and assign the corresponding color.
-		Print the entered color code and the corresponding color name.
-
-		Sample Input / Output:
-		Input:
-		Enter Color code character: g
-		Output:
-		Color Code g is GREEN
-
-	 */
-
+public class Main 
+{
+    public static void main(String[] args) 
+	{
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Color code character: ");
+        char color = sc.next().charAt(0);
+        switch (Character.toLowerCase(color)) 
+	{
+            case 'r':
+                System.out.println("Color Code " + color + " is RED");
+                break;
+            case 'g':
+                System.out.println("Color Code " + color + " is GREEN");
+                break;
+            case 'b':
+                System.out.println("Color Code " + color + " is BLUE");
+                break;
+            default:
+                System.out.println("Color Code " + color + " is BLACK");
+        }
+        sc.close();
+    }
 }
