60b9ef5eea5c4d352f8a9b74fe57c539c01ca251
harish30m <24mca32@kgisliim.ac.in>
2025-06-12 20:27:26 +0530

    Update Q15_HeatAnalysis.java

--- Problem Solving in Java/Q15_HeatAnalysis.java
+++ Problem Solving in Java/Q15_HeatAnalysis.java
@@ -1,29 +1,26 @@
-package basic.to.intermediate;
+import java.util.Scanner;
 
-public class Q15_HeatAnalysis {
-	
-	/**
-	 *  This program uses a switch statement to examine the value of 
-	 an integer called flag. Based on its value, it prints one of the
-	 following messages:
-		HOT if the flag has the value 1.
-		LUKE WARM if the flag has the value 2.
-		COLD if the flag has the value 3.
-		OUT OF RANGE if the flag has any other value.
-		
-	 Problem Flow:
-	Prompt the user to input an integer value for the flag.
-	Use a switch statement to check the value of the flag and assign the correct heat status.
-	Print the flag value and its corresponding heat status.
-	
-	Sample Input / Output:
-	Input:
-	Enter Heat code (1-3): 2
-
-	Output:
-	Flag 2 is LUKE WARM
-
-
-	 */
-
+public class Main 
+{
+    public static void main(String[] args) 
+	{
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Heat code (1-3): ");
+        int flag = sc.nextInt();
+        switch (flag) 
+	{
+            case 1:
+                System.out.println("Flag " + flag + " is HOT");
+                break;
+            case 2:
+                System.out.println("Flag " + flag + " is LUKE WARM");
+                break;
+            case 3:
+                System.out.println("Flag " + flag + " is COLD");
+                break;
+            default:
+                System.out.println("Flag " + flag + " is OUT OF RANGE");
+        }
+        sc.close();
+    }
 }
