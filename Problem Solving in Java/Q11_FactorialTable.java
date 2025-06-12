a232e8cd8c763895303fc3878324469d44a4eaad
harish30m <24mca32@kgisliim.ac.in>
2025-06-12 20:21:53 +0530

    Update Q11_FactorialTable.java

--- Problem Solving in Java/Q11_FactorialTable.java
+++ Problem Solving in Java/Q11_FactorialTable.java
@@ -1,29 +1,22 @@
-package basic.to.intermediate;
+import java.util.Scanner;
 
-public class Q11_FactorialTable {
-	
-	/**
-	 * This program reads a number n from the user and prints the 
-	 factorial of numbers from 1 to n in a table format.
-	 
-	 Problem Flow:
-	Read n from user
-	Start fact = 1
-	Loop from 1 to n
-	Multiply fact *= i
-	Print each number with its factorial
-	
-	Sample Output:
-	Input:
-	Enter the N value: 5
+public class Main 
+{
+    public static void main(String[] args) 
+	{
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number to generate factorial table: ");
+        int num = sc.nextInt();
 
-	Output:
-	N   |   Factorial
-	--------------------
-  	1  |            1
-  	2  |            2
-  	3  |            6
-  	4  |           24
-  	5  |          120
-	 */
+        for (int i = 1; i <= num; i++) 
+	{
+            long fact = 1;
+            for (int j = 1; j <= i; j++) 
+	    {
+                fact *= j;
+            }
+            System.out.println(i + "! = " + fact);
+        }
+        sc.close();
+    }
 }
