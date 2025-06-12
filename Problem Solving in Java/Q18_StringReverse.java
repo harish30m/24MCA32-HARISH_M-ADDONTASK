ce5a29b95e74cbb858301a1c2cdffbc211c48e9c
harish30m <24mca32@kgisliim.ac.in>
2025-06-12 20:30:26 +0530

    Update Q18_StringReverse.java

--- Problem Solving in Java/Q18_StringReverse.java
+++ Problem Solving in Java/Q18_StringReverse.java
@@ -1,26 +1,19 @@
-package basic.to.intermediate;
+import java.util.Scanner;
 
-public class Q18_StringReverse {
-	
-	/**
-	 *  This program takes a string (text) and creates its reverse 
-	 version in another string (reverseText). Each character from the 
-	 original string is added in reverse order to the new string.
-	 
-	 Flow Summary
-	Read the input string
-	Loop from last character to first
-	Use comma operator in loop (i--, j++)
-	Add each character to reverseText
-	Print the reversed output
-	
-	Sample I/O
-	Input:
-	Enter the string below:
-	hello
-	Output:
-	Reversed Text: olleh
-
-	 */
-
+public class Main 
+{
+    public static void main(String[] args) 
+	{
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string below: ");
+        String text = sc.nextLine();
+        String reverseText = "";
+        for (int i = text.length() - 1; i >= 0; i--) 
+	{
+            reverseText += text.charAt(i);
+        }
+        System.out.println("Reversed Text: " + reverseText);
+        sc.close();
+    }
 }
+
