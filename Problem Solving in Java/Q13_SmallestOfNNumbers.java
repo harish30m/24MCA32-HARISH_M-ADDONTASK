bf0968c76982be5109d2fee4bc0ea2a3fdc2d14f
harish30m <24mca32@kgisliim.ac.in>
2025-06-12 20:24:29 +0530

    Update Q13_SmallestOfNNumbers.java

--- Problem Solving in Java/Q13_SmallestOfNNumbers.java
+++ Problem Solving in Java/Q13_SmallestOfNNumbers.java
@@ -1,31 +1,23 @@
-package basic.to.intermediate;
+import java.util.Scanner;
 
-public class Q13_SmallestOfNNumbers {
-	
-	/**
-	 * This program first reads how many numbers the user wants to enter 
-	 (N). Then it reads those N numbers and finds the smallest among 
-	 them.
-	 
-	 Problem Flow:
-	Get total number of inputs N
-	Read first number as smallest
-	Loop remaining N-1 times
-	Compare each number with smallest
-	Update smallest if a smaller number is found
-	Print final smallest number
-	
-	Sample Input / Output:
-	Input:
-	Enter how many numbers you want to input: 5
-	Enter number 1: 22
-	Enter number 2: 18
-	Enter number 3: 31
-	Enter number 4: 6
-	Enter number 5: 27
-	Output:
-	The smallest number is: 6
-
-	 */
-
+public class Main 
+{
+    public static void main(String[] args) 
+	{
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter how many numbers you want to input: ");
+        int n = sc.nextInt();
+        System.out.print("Enter number 1: ");
+        int smallest = sc.nextInt();
+        for (int i = 2; i <= n; i++) 
+            System.out.print("Enter number " + i + ": ");
+            int num = sc.nextInt();
+            if (num < smallest) 
+	    {
+                smallest = num;
+            }
+        }
+        System.out.println("The smallest number is: " + smallest);
+        sc.close();
+    }
 }
