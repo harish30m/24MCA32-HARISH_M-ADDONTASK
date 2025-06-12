1c999018deb4674218b63988ece10837f93e68d3
harish30m <24mca32@kgisliim.ac.in>
2025-06-12 20:29:40 +0530

    Update Q17_IceWaterSteam.java

--- Problem Solving in Java/Q17_IceWaterSteam.java
+++ Problem Solving in Java/Q17_IceWaterSteam.java
@@ -1,25 +1,25 @@
-package basic.to.intermediate;
+import java.util.Scanner;
 
-public class Q17_IceWaterSteam {
-	
-	/**
-	 *  This program checks the temperature value (temp) and determines 
-	 the physical state of water:
-		If temp < 0 → "ICE"
-		If temp is between 0 and 100 (inclusive) → "WATER"
-		If temp > 100 → "STEAM"
-		
-		Flow Summary:
-		Get user input for temperature
-		Use if-else to decide the state of water
-		Display the result to the user
-		
-		Sample I/O:
-		Input:
-		Enter the water temperature: 120
-		Output:
-		Water status is STEAM for the Temperature 120.00
-
-	 */
-
+public class Main 
+{
+    public static void main(String[] args) 
+	{
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the water temperature: ");
+        double temp = sc.nextDouble();
+        if (temp < 0) 
+	{
+            System.out.printf("Water status is ICE for the Temperature %.2f\n", temp);
+        } 
+	else if (temp <= 100) 
+	{
+            System.out.printf("Water status is WATER for the Temperature %.2f\n", temp);
+        } 
+	else 
+	{
+            System.out.printf("Water status is STEAM for the Temperature %.2f\n", temp);
+        }
+        sc.close();
+    }
 }
+
